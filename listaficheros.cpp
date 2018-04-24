445986f88c14c92a4fe84c5b6f6ec4d5325ecc9b
StyLerZ <alu0101053318.ull.edu.es>
2018-04-24 22:34:58 +0000

    Added listaficheros.cpp

---
+++ listaficheros.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
+int main(void) {
+  int inx = 0;
+  ifstream inFile;
+  vector<string> fileVector;
+
+  //system("ls *.gr");
+  system("ls > filelist.txt");
+  inFile.open("filelist.txt");
+
+  string aux;
+  while (inFile >> aux)
+    if (aux.substr(aux.find_last_of(".")) == ".gr")
+      fileVector.push_back(aux);
+
+  while (inx < fileVector.size()) {
+    for (int len = 0; len < 4 && len < fileVector.size(); ++len) {
+      cout << fileVector[inx] << " ";
+      ++inx;
+    }
+    cout << endl;
+  }   
+
+  return 0;
+}
