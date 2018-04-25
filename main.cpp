a95b19c9bbe88f8f16e3ac170ccdafe83d40aaa6
StyLerZ <33236426+StyLerZ@users.noreply.github.com>
2018-04-25 15:04:18 +0100

    Updated show_files()

--- main.cpp
+++ main.cpp
@@ -139,24 +139,5 @@ short unsigned print_menu(bool mode) {
 }
 
 void show_files(void) {
-  int inx = 0;
-  ifstream inFile;
-  vector<string> fileVector;
-
-  //system("ls *.gr");
-  system("ls > filelist.txt");
-  inFile.open("filelist.txt");
-
-  string aux;
-  while (inFile >> aux)
-    if (aux.substr(aux.find_last_of(".")) == ".gr")
-      fileVector.push_back(aux);
-
-  while (inx < fileVector.size()) {
-    for (int len = 0; len < 4 && len < fileVector.size(); ++len) {
-      cout << fileVector[inx] << " ";
-      ++inx;
-    }
-	  
-  cout << endl;
+  system("ls *.gr");
 }
